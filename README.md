d5bbf8e0cd83de4f4c95b2041c0b876cee0e9849
present-cjn <1350321603@qq.com>
2023-03-07 14:12:41 +0800

    add data/

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # Mosse
+this is the **python** implementation of the - [Visual object tracking using adaptive correlation filters](https://ieeexplore.ieee.org/document/5539960/).
 
 ## Prepare
 ```shell
 pip -r install requirements.txt
 ```
 ## Run
+demo
 ```python
 python tracking.py
+```
+your data
+```python
+python tracking.py --path 'data_path'
 ```
 
 ## Result
--- tracking.py
+++ tracking.py
@@ -7,8 +7,14 @@ File:     tracking.py
 Describe: 
 """
 from Mosse import Mosse
+import argparse
+
+parser = argparse.ArgumentParser(description='命令行参数')
+parser.add_argument('--path', '-p', default='./data/david', type=str, help='视频的地址')
+
+args = parser.parse_args()
 
 if __name__ == "__main__":
-    path = './data/david'
+    path = args.path
     tracker = Mosse(path=path)
     tracker.start_tracking()
